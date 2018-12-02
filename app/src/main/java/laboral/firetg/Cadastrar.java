1df2605235df7a9bddd7556bb477294615e92259
Araki Takehiro Vinicius <vinicius.araki@fatecitapetininga.edu.br>
2018-12-02 03:04:54 -0200

    ajustes na exception e nome da classe de controle2

--- app/src/main/java/laboral/firetg/Cadastrar.java
+++ app/src/main/java/laboral/firetg/Cadastrar.java
@@ -5,10 +5,21 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import laboral.firetg.control.ControlAluno;
+import laboral.firetg.control.exc.AlunoException;
+import laboral.firetg.model.aluno;
 
 public class Cadastrar extends AppCompatActivity {
 
-    private Button jbtnCancelar;
+    private Button jbtnCancelar,jbtnSalvar;
+    private EditText jedtNome,jedtObservacao;
+    private FirebaseFirestore db;
+    aluno a = new aluno();
+    ControlAluno ca=new ControlAluno();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +30,10 @@ public class Cadastrar extends AppCompatActivity {
 
     private void inicializa_componentes(){
         jbtnCancelar=findViewById(R.id.btnCancelar);
+        jbtnSalvar=findViewById(R.id.btnSalvar);
+        jedtNome=findViewById(R.id.edtNome);
+        jedtObservacao=findViewById(R.id.edtObservacao);
+        db=FirebaseFirestore.getInstance();
 
     }
     private void standby() {
@@ -26,6 +41,20 @@ public class Cadastrar extends AppCompatActivity {
             public void onClick(View v) {
                 Intent it = new Intent(getApplicationContext(), Principal.class);
                 startActivity(it);
+            }
+        });
+
+        jbtnSalvar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                a.setNome(jedtNome.getText().toString());
+                a.setObservacao(jedtObservacao.getText().toString());
+                try {
+                    ca.setAluno(a);
+                    ca.registra();
+                } catch (AlunoException e) {
+                    e.printStackTrace();
+                }
             }
         });
     }
