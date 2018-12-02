c97c120bc22ce301819073adad675d6d91ae4206
Dayvison <dayvison.vaz@fatecitapetininga.edu.br>
2018-12-02 03:08:58 -0200

    adicionado tela cadastrar, chamada, pesquisar; criado botoes e labels da tela cadastrar; botões da tela principal chamando outras telas.

--- app/src/main/java/laboral/firetg/Pesquisar.java
+++ app/src/main/java/laboral/firetg/Pesquisar.java
@@ -1,13 +1,33 @@
 package laboral.firetg;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Pesquisar extends AppCompatActivity {
 
+    private Button jbtnCancelar, jbtnPesquisar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pesquisar);
+        inicializa_componentes();
+        standby();
+    }
+
+    private void inicializa_componentes(){
+        jbtnCancelar=findViewById(R.id.btnCancelar);
+        jbtnPesquisar=findViewById(R.id.btnPesquisar);
+
+    }
+    private void standby() {
+        jbtnCancelar.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent it = new Intent(getApplicationContext(), Principal.class);
+                startActivity(it);
+            }
+        });
     }
 }
